afab19296ee4a87fd2f056a89baf60233cb6d54d
João Pedro <joaopedro.oliveirads@gmail.com>
2024-11-01 08:20:25 -0300

    fix: restructuring files and project structure

---
+++ sistema-de-matriculas-vk/src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+    return (
+      <>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Recusandae quas nemo ullam fuga tempora voluptatum, suscipit id, reiciendis maiores saepe quo vero aperiam, atque minima similique eaque iure? Unde, atque.</p>
+      </>
+    )
+}
