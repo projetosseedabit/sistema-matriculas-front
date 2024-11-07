f4bf7faa16e143fcbbcfe05e9845396069cb6b7c
João Pedro <joaopedro.oliveirads@gmail.com>
2024-11-06 22:19:56 -0300

    fix: navbar proportions

--- sistema-matriculas-front/src/app/components/header.tsx
+++ sistema-matriculas-front/src/app/components/header.tsx
@@ -3,10 +3,8 @@ import Image from 'next/image';
 
 const Header:React.FC = () => {
     return (
-        <header className="bg-blue-800">
-            <div className="container mx-auto flex items-center p-40">
-                <Image src='/logoAzul.jpg' alt="Logo Azul - Isolada de Redação VK" width={150} height={150}/>     
-            </div>
+        <header className="bg-blue-800 w-full container mx-auto flex items-center p-4 pl-16">
+            <Image src='/logoAzul.jpg' alt="Logo Azul - Isolada de Redação VK" className="w-[150px] h-[88px]" width={1080} height={1080}/>     
         </header>
     );
 };
