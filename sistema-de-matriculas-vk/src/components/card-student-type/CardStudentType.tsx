c837adee69ca390c45b3f2d416854fa94cca7ada
GrazielaFelix <grazielafelix.dev@gmail.com>
2024-11-07 22:47:31 -0300

    feat: criar componente de card de tipo de estudante

---
+++ sistema-de-matriculas-vk/src/components/card-student-type/CardStudentType.tsx
@@ -0,0 +1,11 @@
+export default function CardStudentType() {
+    return (
+      <>
+        <div className="border-solid border-2 bg-indigo-500">
+            <input type="checkbox" className="bg-blue-200"></input>
+            <p>Sou maior de idade, sou meu próprio responsável financeiro e quero me inscrever para a Isolada de Redação VK.</p>
+            <button className="bg-sky-500/100">test</button>
+        </div>
+      </>
+    )
+}
