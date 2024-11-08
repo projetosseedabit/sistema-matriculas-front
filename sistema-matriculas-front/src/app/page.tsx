762f9c5533a9abf4d59aef0506bf618fad14e218
heitorleonny <heitorleonnyyt1234@gmail.com>
2024-11-07 23:37:11 -0300

    feat: add Button Component

--- sistema-matriculas-front/src/app/page.tsx
+++ sistema-matriculas-front/src/app/page.tsx
@@ -1,3 +1,4 @@
+import { Button } from "./components/button/Button";
 import Header from "./components/header";
 
 export default function RootLayout({
@@ -9,6 +10,12 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <Header />
+        <div className="max-w-screen-xl mx-auto px-4">
+        <div className="flex justify-between max-w-screen-xl mx-auto px-4">
+          <Button label="Voltar" color="bg-sky-950 hover:bg-sky-900"/>
+          <Button label="Avançar" color="bg-yellow-500 hover:bg-yellow-400"/>
+        </div>
+        </div>
         <main className="min-h-screen">{children}</main>
       </body>
     </html>
