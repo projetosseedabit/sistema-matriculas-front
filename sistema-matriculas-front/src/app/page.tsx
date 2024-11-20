b1f986f1badacf679da079461091f32f3804d7b3
GrazielaFelix <grazielafelix.dev@gmail.com>
2024-11-19 22:57:49 -0300

    feat: componente de card de turma

--- sistema-matriculas-front/src/app/page.tsx
+++ sistema-matriculas-front/src/app/page.tsx
@@ -1,15 +1,25 @@
-import { CardStudentType } from "@/components/card-student-type/CardStudentType";
+'use client'
+import { ClassCard } from "@/components/class-card/class-card";
 import Header from "../components/header";
+import { useState } from "react";
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const [selectedOption, setSelectedOption] = useState<string | null>(null);
+
+  const handleSelection = (value: string) => {
+    setSelectedOption(value);
+    console.log(value);
+  };
   return (
     <>
       <Header />
-      
+      <ClassCard  value="underage"
+      checked={selectedOption === "underage"}
+      onChange={handleSelection} dayOfWeek={"Segunda-feira"} modality={"Presencial"} id={"0"} price={0} totalVacancies={45} vacanciesFilled={0} startTime={"19h"} endTime={"20h"}></ClassCard>
       <main className="min-h-screen">{children}</main>
     </>
   );
